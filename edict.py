1fb82f21546500da57bef82e09fa0f8f70cffccd
dasu <desusp@gmail.com>
2015-03-20 14:53:24 -0400

    Create edict.py
    update edict to willie standards...also made some type of change that either made it better or worse.

---
+++ edict.py
@@ -0,0 +1,32 @@
+from urllib.request import urlopen, quote
+from bs4 import BeautifulSoup
+import willie
+@willie.module.commands('edict')
+@willie.module.example('.edict word/character')
+def edict(bot, trigger):
+    if not trigger.group(2):
+        return bot.say("Please enter a word.")
+    i = trigger.group(2)
+    try:
+        i.encode('ascii')
+        print(i)
+        x = urlopen("http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi?1ZDJ{0}".format(i))
+        c = x.read()
+        bs = BeautifulSoup(c)
+        #print(bs)
+        if bs.pre:
+            res = bs.pre.contents[0].splitlines()[1]
+            #print(res)
+        else:
+            res = "No matches found."
+        bot.say(res)
+    except:
+        print(i)
+        x = urlopen("http://www.csse.monash.edu.au/~jwb/cgi-bin/wwwjdic.cgi?1ZIK{0}".format(quote(i)))
+        c = x.read()
+        bs = BeautifulSoup(c)
+        if bs.li:
+            res = bs.li.contents[0]
+        else:
+            res = "No matches found."
+        bot.say(res)
