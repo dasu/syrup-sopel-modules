3e464fd66be3a9d8b16529b07f8c99df231f3324
dasu <desusp@gmail.com>
2015-03-20 16:25:01 -0400

    Create plugdj.py
    updated to willie standards.  this plugin is almost as bad as weeb.py

---
+++ plugdj.py
@@ -0,0 +1,9 @@
+import willie
+
+@willie.module.commands('partypoi','djpoi')
+def djpoi(bot,trigger):
+	bot.say("さあ、素敵なパーティーし https://plug.dj/4045415754446389448")
+
+@willie.module.commands('party','dj')
+def dj(bot,trigger):
+        bot.say("It's a party! https://plug.dj/4045415754446389448")
