cc46b2f40e8a86ba274a1b380590ad0423460205
dasu <desusp@gmail.com>
2015-06-15 12:00:42 -0400

    Update mtg.py

--- mtg.py
+++ mtg.py
@@ -1,6 +1,8 @@
+
 #card name, card type, mana cost, card effect, power +
 #toughness if the card type is a creature
-from urllib.request import urlopen
+#from urllib.request import urlopen
+import requests
 import json
 import willie
 
@@ -9,13 +11,17 @@ def mtg(bot,trigger):
     if not trigger.group(2):
         return bot.say("Please enter a card name.")
     i = trigger.group(2)
-    x = urlopen('http://api.mtgdb.info/search/{0}'.format(i))
-    c = x.read()
-    try:
-        js = json.loads(c.decode('utf-8'))[0]
-    except:
+    x = requests.get('https://api.deckbrew.com/mtg/cards?name={0}'.format(i))
+    c = x.json()
+    if not c:
         return bot.say('Card not found.')
-    if js['type'] == 'Creature':
-        bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: {3}, Power: {4}, Toughness: {5}'.format(js['name'], js['type'], js['manaCost'], js['description'], js['power'], js['toughness']))
+        #try:
+    #    js = json.loads(c.decode('utf-8'))[0]
+    #except:
+    #    return bot.say('Card not found.')
+    js = c[0]
+
+    if js['types'] == ['creature']:
+        bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: {3}, Power: {4}, Toughness: {5}'.format(js['name'], js['types'], js['cost'], js['text'], js['power'], js['toughness']))
     else:
-        bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: "{3}"'.format(js['name'], js['type'], js['manaCost'], js['description']))
+        bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: "{3}"'.format(js['name'], js['types'], js['cost'], js['text']))
