6552af62136f2456a6b93d9779b6c8ccee5f33ce
dasu <desusp@gmail.com>
2015-10-29 16:12:51 -0400

    add url callback support
    and a couple of other small changes

--- vndb.py
+++ vndb.py
@@ -1,13 +1,28 @@
+#code based off https://github.com/theappleman/pyvndb
+#this is a mess and can be refactored significantly
+
 import json
 from socket import socket as ss
-import willie
+import sopel
+import re
+from sopel.tools import SopelMemory
+
+vndbregex = re.compile('.*(https?:\/\/vndb.org\/v(.*?((?=[\s])|$)))')
+
+def setup(bot):
+    if not bot.memory.contains('url_callbacks'):
+        bot.memory['url_callbacks'] = SopelMemory()
+    bot.memory['url_callbacks'][vndbregex] = vndbirc
+
+def shutdown(bot):
+    del bot.memory['url_callbacks'][vndbregex]
 
 def login(sock):
     sock.connect(("api.vndb.org", 19534))
-    test = bytes("login {\"protocol\":1,\"client\":\"syrup\",\"clientver\":0.8}\x04", "utf-8")
+    test = bytes("login {\"protocol\":1,\"client\":\"syrup\",\"clientver\":0.9}\x04", "utf-8")
     sock.sendall(test)
     recv(sock)
-    
+
 def recv(sock):
     data = sock.recv(4096)
     if not str(data, "utf-8").endswith("\x04"):
@@ -19,23 +34,25 @@ def gettags(tags):
     tagsjs = json.loads(json_data)
     tah = []
     for tag in tags:
-        if tag[2] == 0:
-            if tag[1] >= 2.95:
-                for b in tagsjs:
-                    if b['id'] == tag[0]:
-                        tah.append(b['name'])
+        if tag[2] == 0 and tag[1] >= 2.95:
+            for b in tagsjs:
+                if b['id'] == tag[0]:
+                    tah.append(b['name'])
     tags2 = ", ".join(tah)
     return tags2
 
-@willie.module.commands('vndb','vn')
-@willie.module.example('.vn or .vndb visualnoveltitle')
-def vndb(bot, trigger):
+def getlength(vnlength):
     length = {}
     length[1] = "Very short (< 2 hours)"
     length[2] = "Short (2 - 10 hours)"
     length[3] = "Medium (10 - 30 hours)"
     length[4] = "Long (30 - 50 hours)"
     length[5] = "Very Long (> 50 hours)"
+    return length[vnlength]
+
+@sopel.module.commands('vndb','vn')
+@sopel.module.example('.vn or .vndb visualnoveltitle')
+def vndb(bot, trigger):
     sock = ss()
     if not trigger.group(2):
         return bot.say("Enter a VN name to search.")
@@ -59,7 +76,7 @@ def vndb(bot, trigger):
             if not js['items'][0]['length']:
                 vnlength = 'Unknown'
             else:
-                vnlength = length[js['items'][0]['length']]
+                vnlength = getlength(js['items'][0]['length'])
             if not js['items'][0]['released']:
                 released = 'Unknown'
             else:
@@ -68,3 +85,34 @@ def vndb(bot, trigger):
         else:
             return bot.say("No results.")
     sock.close()
+    
+def vndbirc(bot, trigger, match = None):
+match = match or trigger
+request = "get vn basic,details,stats,tags (id = {0})\x04".format(match.group(2))
+sock = ss()
+test = bytes(request, "utf-8")
+login(sock)
+sock.sendall(test)
+vn = recv(sock)
+if str(vn, "utf-8")[0:5] == 'error':
+    return bot.say("An error occured.")
+else:
+    js = json.loads((str(vn, "utf-8"))[8:-1])
+    if js['items']:
+        title = js['items']
+        tags = js['items'][0]['tags']
+        tags2 = gettags(tags)
+        if not tags2:
+            tags2 = "None"
+        if not js['items'][0]['length']:
+            vnlength = 'Unknown'
+        else:
+            vnlength = getlength(js['items'][0]['length'])
+        if not js['items'][0]['released']:
+            released = 'Unknown'
+        else:
+            released = js['items'][0]['released']
+        return bot.say('{0} | Released: {1}, Length: {2}, Rating: {3} Tags: {4}'.format(js['items'][0]['title'],released,vnlength,js['items'][0]['rating'],tags2))
+    else:
+        pass
+sock.close()
