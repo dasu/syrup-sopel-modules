60b1682198e21052ac9c84bceb293b1256ae66e3
dasu <desusp@gmail.com>
2015-11-11 10:02:34 -0500

    Create dubtrack.py

---
+++ dubtrack.py
@@ -0,0 +1,21 @@
+import requests
+import sopel
+import json
+
+@sopel.module.commands('partypoi','djpoi','dubpoi')
+def djpoi(bot,trigger):
+    #bot.say("")
+    x = requests.get("http://api.dubtrack.fm/room/563eabe1d4c45a1e00caa25a").json()
+    if not x['data']['currentSong']:
+        bot.say("さあ、素敵なパーティーしましょう https://www.dubtrack.fm/join/pancakes")
+    else:
+        bot.say("Current song: {0} Listeners: {1} | https://www.dubtrack.fm/join/pancakes".format(x['data']['currentSong']['name'],x['data']['activeUsers']))
+
+@sopel.module.commands('party','dj', 'dub','dubtrack')
+def dj(bot,trigger):
+    #bot.say("The party is dead.")
+    x = requests.get("http://api.dubtrack.fm/room/563eabe1d4c45a1e00caa25a").json()
+    if not x['data']['currentSong']:
+        bot.say("It's a party! https://www.dubtrack.fm/join/pancakes")
+    else:
+        bot.say("Current song: {0} Listeners: {1} | https://www.dubtrack.fm/join/pancakes".format(x['data']['currentSong']['name'],x['data']['activeUsers']))
