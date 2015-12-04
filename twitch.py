f87d4c723b5a0af896088902f35794ffaa8e9b22
dasu <desusp@gmail.com>
2015-12-04 14:49:05 -0500

    add twitch url callback
    holy shit this really needs to be cleaned up.

--- twitch.py
+++ twitch.py
@@ -1,5 +1,7 @@
 import requests
 import sopel
+import re
+from sopel.tools import SopelMemory
 
 # TODO: Make these config options c:
 announce_chan = "#pancakes"
@@ -29,6 +31,16 @@ hstreamers = [
 'agriks'
 ]
 
+twitchregex = re.compile('(.*(https?:\/\/)(www\.)?twitch.tv\/.*?((?=[\s])|$))')
+
+def setup(bot):
+    if not bot.memory.contains('url_callbacks'):
+        bot.memory['url_callbacks'] = SopelMemory()
+    bot.memory['url_callbacks'][twitchregex] = twitchirc
+
+def shutdown(bot):
+    del bot.memory['url_callbacks'][twitchregex]
+
 currently_streaming = {}
 currently_hstreaming = {}
 currently_ystreaming = {}
@@ -43,29 +55,21 @@ def monitor_streamers(bot):
     streamer_game = streamer["channel"]["game"]
     streamer_url = streamer["channel"]["url"]
     streamer_viewers = streamer["viewers"]
-
+    
     if streamer_name not in currently_streaming:
       currently_streaming[streamer_name] = streamer_game, {'cooldown': 0}
-      results.append("%s just went live playing %s! (%s - %s viewer%s)" % (streamer_name,
-                                                                          streamer_game,
-                                                                          streamer_url,
-                                                                          streamer_viewers,
+      results.append("%s just went live playing %s! (%s - %s viewer%s)" % (streamer_name, 
+                                                                          streamer_game, 
+                                                                          streamer_url, 
+                                                                          streamer_viewers, 
                                                                           "s" if streamer_viewers != 1 else ""))
-    elif streamer_game != currently_streaming[streamer_name][0]:
-      currently_streaming[streamer_name] = streamer_game, {'cooldown': 0}
-      results.append("%s just started playing %s! (%s - %s viewer%s)" % (streamer_name,
-                                                                        streamer_game,
-                                                                        streamer_url,
-                                                                        streamer_viewers,
-                                                                        "s" if streamer_viewers != 1 else ""))
-
     streaming_names.append(streamer_name)
 
   if results:
-    bot.msg(announce_chan, ", ".join(results))
+    bot.msg(announce_chan, ", ".join(results))  
 
   # Remove people who stopped streaming
-  for streamer in currently_streaming.keys():
+  for streamer in list(currently_streaming):
     if streamer not in streaming_names:
       currently_streaming[streamer][1]['cooldown'] += 10
     if currently_streaming[streamer][1]['cooldown'] > 130:
@@ -81,21 +85,17 @@ def monitor_streamers(bot):
       hstreamer_game = hstreamer["category_name"]
       hstreamer_url = hstreamer["channel"]["channel_link"]
       hstreamer_viewers = hstreamer["media_views"]
-
+    
       if hstreamer_name not in currently_hstreaming:
         currently_hstreaming[hstreamer_name] = hstreamer_game, {'cooldown': 0}
         hresults.append("%s just went live playing %s! (%s - %s viewer%s)" % (hstreamer_name,hstreamer_game,hstreamer_url,hstreamer_viewers,"s" if hstreamer_viewers != 1 else ""))
-
-      elif hstreamer_game != currently_hstreaming[hstreamer_name][0]:
-        currently_hstreaming[hstreamer_name] = hstreamer_game, {'cooldown': 0}
-        hresults.append("%s just started playing %s! (%s - %s viewer%s)" % (hstreamer_name,hstreamer_game,hstreamer_url,hstreamer_viewers,"s" if hstreamer_viewers != 1 else ""))
 
       hstreaming_names.append(hstreamer_name)
 
   if hresults:
     bot.msg(announce_chan, ", ".join(hresults))
-
-  for hstreamer in currently_hstreaming.keys():
+  
+  for hstreamer in list(currently_hstreaming):
     if hstreamer not in hstreaming_names:
       currently_hstreaming[hstreamer][1]['cooldown'] += 10
     if currently_hstreaming[hstreamer][1]['cooldown'] > 130:
@@ -103,34 +103,29 @@ def monitor_streamers(bot):
 
   ystreaming_names = []
   yresults = []
-  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=enteryourkeyhere').json()
+  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=ENTERKEYHERE').json()
 
   if ystreaming["items"]:
     ystreamer_name = "repppie"
     ystreamer_url = "http://youtube.com/user/repppie/live"
     ystreamer_title = ystreaming["items"][0]["snippet"]["title"]
-
+    
     if ystreamer_name not in currently_ystreaming:
       currently_ystreaming[ystreamer_name] = ystreamer_title, {'cooldown': 0}
       yresults.append("%s just went live playing %s! ( %s )" % (ystreamer_name, ystreamer_title, ystreamer_url))
-
-    elif ystreamer_title != currently_ystreaming[ystreamer_name][0]:
-      currently_ystreaming[ystreamer_name] = ystreamer_title, {'cooldown': 0 }
-      yresults.append("%s just started playing %s! ( %s )" % (ystreamer_name, ystreamer_title, ystreamer_url))
-
-
-
+      
+      
+      
     ystreaming_names.append(ystreamer_name)
-
+    
   if yresults:
     bot.msg(announce_chan, ", ".join(yresults))
-
-  for ystreamer in currently_ystreaming.keys():
+    
+  for ystreamer in list(currently_ystreaming):
     if ystreamer not in ystreaming_names:
       currently_ystreaming[ystreamer][1]['cooldown'] += 10
     if currently_ystreaming[ystreamer][1]['cooldown'] > 130:
       del currently_ystreaming[ystreamer]
-
 
 @sopel.module.commands('twitchtv','twitch')
 @sopel.module.example('.twitch  or .twitch twitchusername')
@@ -145,16 +140,17 @@ def streamer_status(bot, trigger):
     streamer_game = streamer["channel"]["game"]
     streamer_url = streamer["channel"]["url"]
     streamer_viewers = streamer["viewers"]
-
-    results.append("%s is playing %s (%s - %s viewer%s)" % (streamer_name,
-                                                           streamer_game,
-                                                           streamer_url,
-                                                           streamer_viewers,
+    
+    results.append("%s is playing %s (%s - %s viewer%s)" % (streamer_name, 
+                                                           streamer_game, 
+                                                           streamer_url, 
+                                                           streamer_viewers, 
                                                            "s" if streamer_viewers != 1 else "" ))
   if results:
     bot.say(", ".join(results))
   else:
     bot.say("Nobody is currently streaming.")
+
 
 @sopel.module.commands('hb','hitbox')
 @sopel.module.example('.hb  or .hb twitchusername')
@@ -169,7 +165,7 @@ def hstreamer_status(bot, trigger):
         hstreamer_game = hstreamer["category_name"]
         hstreamer_url = hstreamer["channel"]["channel_link"]
         hstreamer_viewers = hstreamer["media_views"]
-
+    
         hresults.append("%s is playing %s (%s - %s viewer%s)" % (hstreamer_name,
                                                            hstreamer_game,
                                                            hstreamer_url,
@@ -184,7 +180,7 @@ def hstreamer_status(bot, trigger):
 @sopel.module.example('.yg')
 def ystreamer_status(bot, trigger):
 
-  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=enteryourkeyhere').json()
+  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=ENTERKEYHERE').json()
   yresults = []
   if ystreaming["items"]:
     ystreamer_name = "repppie"
@@ -234,7 +230,7 @@ def allstreamer_status(bot, trigger):
                                                            hstreamer_viewers,
                                                            "s" if hstreamer_viewers != 1 else "" ))
 
-  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=enteryourkeyhere').json()
+  ystreaming = requests.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=UCKJTvpLA4kcahLhl5ka-iIw&eventType=live&type=video&key=ENTERKEYHERE').json()
   yresults = []
 
   if ystreaming["items"]:
@@ -246,6 +242,29 @@ def allstreamer_status(bot, trigger):
                                                  ystreamer_title,
                                                  ystreamer_url))
 
+  if results:
+    bot.say(", ".join(results))
+  else:
+    bot.say("Nobody is currently streaming.")
+
+@sopel.module.rule('(.*(https?:\/\/)(www\.)?twitch.tv\/.*?((?=[\s])|$))')
+def twitchirc(bot, trigger, match = None):
+  match = match or trigger
+  streamer_name = (match.group(0)).split("/")[-1]
+  query = streamers if streamer_name is None else streamer_name.split(" ")
+  streaming = requests.get('https://api.twitch.tv/kraken/streams', params={"channel": ",".join(query)}).json()
+  results = []
+  for streamer in streaming["streams"]:
+    streamer_name = streamer["channel"]["name"]
+    streamer_game = streamer["channel"]["game"]
+    streamer_status = streamer["channel"]["status"]
+    streamer_viewers = streamer["viewers"]
+
+    results.append("%s is playing %s [%s] - %s viewer%s" % (streamer_name,
+                                                           streamer_game,
+                                                           streamer_status,
+                                                           streamer_viewers,
+                                                           "s" if streamer_viewers != 1 else "" ))
   if results:
     bot.say(", ".join(results))
   else:
