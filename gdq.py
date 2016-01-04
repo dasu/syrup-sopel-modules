fec1b3dfab5da32dee67624cf0d03c7bce344530
dasu <desusp@gmail.com>
2016-01-04 10:03:01 -0500

    Update gdq.py

--- gdq.py
+++ gdq.py
@@ -1,91 +1,71 @@
-#I have no clue if this will work come JAN 1st...
-import sopel
-import requests
-import json
+#oh god damn it, they changed it back to the old way.
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
 from datetime import datetime, timedelta, date
+import sopel
 
-def getinfo(gdqlist,now):
-    game,runner,category,comment,eta,nextgame,nextrunner,nextcategory,nexteta,nextcomment = '','','','','','','','','',''
-    for item in gdqlist:
-        starttime = item[1]
-        offset = datetime.strptime(item[4], "%H:%M:%S")
+
+def getinfo(run,now):
+    schedule = run.find_all('tr')
+    game,runner,console,comment,eta,nextgame,nextrunner,nextconsole,nexteta,nextcomment = '','','','','','','','','',''
+    for item in schedule:
+        group = item.find_all('td')
+        st = group[0].getText()
+        estfix = timedelta(hours=-5)
+        starttime = datetime.strptime(st, '%Y-%m-%dT%H:%M:%SZ' )
+        starttime = starttime + estfix
+        offset = datetime.strptime(group[3].getText(), "%H:%M:%S")
         endtime = starttime + timedelta(hours = offset.hour, minutes = offset.minute, seconds=offset.second)
         if starttime < now and endtime > now:
-            game = item[0]
-            category = item[2]
-            runner = item[3]
-            eta = item[4]
-            comment = item[6]
+            game = group[1].getText()
+            runner = group[2].getText()
+            console = group[3].getText()
+            comment = group[6].getText()
+            eta = group[4].getText()
         if starttime > now:
-            nextgame = item[0]
-            nextrunner = item[3]
-            nextcategory = item[2]
-            nexteta = item[4]
-            nextcomment = item[6]
+            nextgame = group[1].getText()
+            nextrunner = group[2].getText()
+            nextconsole = group[3].getText()
+            nexteta = group[4].getText()
+            nextcomment = group[6].getText()
             break
         else:
             nextgame = 'done'
             nextrunner = 'done'
-    return(game, runner, category, comment, eta, nextgame, nextrunner, nexteta, nextcategory, nextcomment)
+    return (game, runner, console, comment, eta, nextgame, nextrunner, nexteta, nextconsole, nextcomment)
+            
 
 @sopel.module.commands('gdq','sgdq','agdq')
-def gdq(bot,trigger):
-    gdqlist = []
-    row = ""
-    dati, game, runners, time, category, estimate, setup, comment = '','','','','','','',''
-    url ='https://spreadsheets.google.com/feeds/cells/1_gibuPSqCLOY8HruYJ15OYGGkFfth0IevIi7fE9Xp2E/1/public/values?prettyprint=true&alt=json'
-    r = requests.get(url).json()
-
-    year = datetime.today().year+1 if datetime.today().month is 12 else datetime.today().year
+def gdq(bot, trigger):
+    url = 'https://gamesdonequick.com/schedule'
+    req = urllib.request.Request(url,data=None,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    try:
+        x = urllib.request.urlopen(req)
+    except:
+        now = datetime.now()
+        delta = datetime(2016,1,3,12,00) - now
+        return bot.say("GDQ is {0} days away (January 03)".format(delta.days))
+    c = (x.read().decode('utf-8'))
+    bs = BeautifulSoup(c)
+    run = bs.find("tbody",{"id":"runTable"})
     now = datetime.now()
-    for cell in r['feed']['entry']:
-        if cell['gs$cell']['col'] == '1':
-            if cell['gs$cell']['$t']:
-                try:
-                    datetime.strptime(cell['gs$cell']['$t'], '%A %m/%d')
-                    date = cell['gs$cell']['$t']
-                except:
-                    pass
-        if cell['gs$cell']['col'] == '2':
-            if row<cell['gs$cell']['row']:
-                if (game != 'Game') and (game != ''):
-                    gdqlist+=[[game, dati, category, runners, estimate, setup, comment]]
-            row = cell['gs$cell']['row']
-            game, runners, time, category, estimate, setup, comment = '','','','','','',''
-            try:
-                time = cell['gs$cell']['$t']
-                dati = datetime.strptime("{} {}, {}".format(date, year, time), "%A %m/%d %Y, %I:%M %p" )
-            except:
-                pass
-        if cell['gs$cell']['col'] == '3':
-            game = cell['gs$cell']['$t']
-        if cell['gs$cell']['col'] == '4':
-            runners = cell['gs$cell']['$t']
-        if cell['gs$cell']['col'] == '5':
-            category = cell['gs$cell']['$t']
-        if cell['gs$cell']['col'] == '6':
-            estimate = cell['gs$cell']['$t']
-        if cell['gs$cell']['col'] == '7':
-            setup = cell['gs$cell']['$t']
-        if cell['gs$cell']['col'] == '8':
-            if cell['gs$cell']['$t']:
-                comment = cell['gs$cell']['$t']
-    (game, runner, category, comment, eta, nextgame, nextrunner, nexteta, nextcategory, nextcomment) = getinfo(gdqlist, now)
-    if now < gdqlist[0][1]:
-        tts = gdqlist[0][1] - now
+    gdqstart = datetime.strptime(run.td.getText(), '%Y-%m-%dT%H:%M:%SZ')
+    (game, runner, console, comment, eta, nextgame, nextrunner, nexteta, nextconsole, nextcomment) = getinfo(run,now)
+    if now < gdqstart:
+        tts = gdqstart - now
         if tts.days <= 3:
-            return bot.say("GDQ is {0} hours away. First game: {1} [{2}] by {3} ETA: {4} ".format(int(tts.total_seconds() // 3600),nextgame, nextcategory, nextrunner, nexteta))
+            return bot.say("GDQ is {0} hours away.  First game: {1} [{2}] by {3} ETA: {4} Comment: {5} ".format(int(tts.total_seconds() // 3600),nextgame, nextconsole, nextrunner, nexteta, nextcomment))
         else:
-            return bot.say("GDQ is {0} days away ({1})".format(tts.days, gdqlist[0][1].strftime('%m/%d/%Y')))
-
+            return bot.say("GDQ is {0} days away ({1})".format(tts.days,gdqstart.strftime('%m/%d/%Y')))
 
     if nextgame == 'done':
-        delta = datetime(2016,6,15,12,00) - now
-        return bot.say("GDQ is {0} days away (June 15)".format(delta.days))
+        delta = datetime(2016,7,5,12,00) - now
+        return bot.say("GDQ is {0} days away (January 03)".format(delta.days))
     if game:
         if comment:
-            bot.say("Current Game: {0} [{1}] by {2} ETA: {3} Comment: {4} | Next Game: {5} by {6}".format(game, category, runner, eta, comment, nextgame, nextrunner))
+            bot.say("Current Game: {0} [{1}] by {2} ETA: {3} Comment: {4} | Next Game: {5} by {6} | http://www.twitch.tv/gamesdonequick".format(game, console, runner, eta, comment, nextgame, nextrunner))
         else:
-            bot.say("Current Game: {0} [{1}] by {2} ETA: {3} | Next Game: {4} by {5}".format(game, category, runner, eta, nextgame, nextrunner))
+            bot.say("Current Game: {0} [{1}] by {2} ETA: {3} | Next Game: {4} by {5} | http://www.twitch.tv/gamesdonequick".format(game, console, runner, eta, nextgame, nextrunner))
     else:
-        bot.say("Current Game: setup?? | Next Game {0} by {1}".format(nextgame, nextrunner))
+        bot.say("Current Game: setup?? | Next Game {0} by {1} | http://www.twitch.tv/gamesdonequick".format(nextgame, nextrunner))
