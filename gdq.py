7a934086be392d49500d649a23f214e6266b5fc0
dasu <desusp@gmail.com>
2016-01-11 12:37:33 -0500

    Update gdq.py

--- gdq.py
+++ gdq.py
@@ -15,8 +15,11 @@ def getinfo(run,now):
         estfix = timedelta(hours=-5)
         starttime = datetime.strptime(st, '%Y-%m-%dT%H:%M:%SZ' )
         starttime = starttime + estfix
-        offset = datetime.strptime(group[3].getText(), "%H:%M:%S")
-        endtime = starttime + timedelta(hours = offset.hour, minutes = offset.minute, seconds=offset.second)
+        try:
+            offset = datetime.strptime(group[3].getText(), "%H:%M:%S")
+            endtime = starttime + timedelta(hours = offset.hour, minutes = offset.minute, seconds=offset.second)
+        except:
+            endtime = datetime(2011,1,1,12,00)
         if starttime < now and endtime > now:
             game = group[1].getText()
             runner = group[2].getText()
@@ -44,8 +47,8 @@ def gdq(bot, trigger):
         x = urllib.request.urlopen(req)
     except:
         now = datetime.now()
-        delta = datetime(2016,1,3,12,00) - now
-        return bot.say("GDQ is {0} days away (January 03)".format(delta.days))
+        delta = datetime(2016,7,15,12,00) - now
+        return bot.say("GDQ is {0} days away (July 15 [estimated])".format(delta.days))
     c = (x.read().decode('utf-8'))
     bs = BeautifulSoup(c)
     run = bs.find("tbody",{"id":"runTable"})
@@ -60,8 +63,8 @@ def gdq(bot, trigger):
             return bot.say("GDQ is {0} days away ({1})".format(tts.days,gdqstart.strftime('%m/%d/%Y')))
 
     if nextgame == 'done':
-        delta = datetime(2016,7,5,12,00) - now
-        return bot.say("GDQ is {0} days away (January 03)".format(delta.days))
+        delta = datetime(2016,7,15,12,00) - now
+        return bot.say("GDQ is {0} days away (July 15 [estimated])".format(delta.days))
     if game:
         if comment:
             bot.say("Current Game: {0} [{1}] by {2} ETA: {3} Comment: {4} | Next Game: {5} by {6} | http://www.twitch.tv/gamesdonequick".format(game, console, runner, eta, comment, nextgame, nextrunner))
