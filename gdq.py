fa350390b2598386f2b356bdee9583164f68edc5
dasu <desusp@gmail.com>
2016-03-31 13:29:45 -0400

    a few improvements here and there

--- gdq.py
+++ gdq.py
@@ -1,10 +1,8 @@
 #oh god damn it, they changed it back to the old way.
-import urllib
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta, date
 import sopel
-
 
 def getinfo(run,now):
     schedule = run.find_all('tr')
@@ -37,23 +35,26 @@ def getinfo(run,now):
             nextgame = 'done'
             nextrunner = 'done'
     return (game, runner, console, comment, eta, nextgame, nextrunner, nexteta, nextconsole, nextcomment)
-            
-
+    
 @sopel.module.commands('gdq','sgdq','agdq')
 def gdq(bot, trigger):
-    url = 'https://gamesdonequick.com/schedule'
-    req = urllib.request.Request(url,data=None,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-    try:
-        x = urllib.request.urlopen(req)
-    except:
-        now = datetime.now()
-        delta = datetime(2016,7,15,12,00) - now
-        return bot.say("GDQ is {0} days away (July 15 [estimated])".format(delta.days))
-    c = (x.read().decode('utf-8'))
-    bs = BeautifulSoup(c)
-    run = bs.find("tbody",{"id":"runTable"})
     now = datetime.now()
-    gdqstart = datetime.strptime(run.td.getText(), '%Y-%m-%dT%H:%M:%SZ')
+    delta = datetime(2016,7,3,12,00) - now
+    textdate = "July 3"
+    url = 'https://gamesdonequick.com/schedule'
+    #req = urllib.request.Request(url,data=None,headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+    try:
+        #x = urllib.request.urlopen(req)
+        x = requests.get(url).content
+    except:
+        return bot.say("GDQ is {0} days away ({1})".format(delta.days,textdate))
+    #c = (x.read().decode('utf-8'))
+    bs = BeautifulSoup(x)
+    run = bs.find("tbody",{"id":"runTable"})
+    try:
+        gdqstart = datetime.strptime(run.td.getText(), '%Y-%m-%dT%H:%M:%SZ')
+    except:
+        return bot.say("GDQ is {0} days away ({1})".format(delta.days, textdate))
     (game, runner, console, comment, eta, nextgame, nextrunner, nexteta, nextconsole, nextcomment) = getinfo(run,now)
     if now < gdqstart:
         tts = gdqstart - now
@@ -63,8 +64,7 @@ def gdq(bot, trigger):
             return bot.say("GDQ is {0} days away ({1})".format(tts.days,gdqstart.strftime('%m/%d/%Y')))
 
     if nextgame == 'done':
-        delta = datetime(2016,7,15,12,00) - now
-        return bot.say("GDQ is {0} days away (July 15 [estimated])".format(delta.days))
+        return bot.say("GDQ is {0} days away ({1} [estimated])".format(delta.days,textdate))
     if game:
         if comment:
             bot.say("Current Game: {0} [{1}] by {2} ETA: {3} Comment: {4} | Next Game: {5} by {6} | http://www.twitch.tv/gamesdonequick".format(game, console, runner, eta, comment, nextgame, nextrunner))
