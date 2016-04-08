46fde490e3746c8e94fac56b72bdc03c4a553a34
dasu <desusp@gmail.com>
2016-04-08 12:48:02 -0400

    removed importing another sopel module

--- sotd.py
+++ sotd.py
@@ -4,8 +4,6 @@ import datetime
 from urllib.parse import urlparse
 from sopel.tools import Identifier
 import re
-from youtoob import fetch_video_info 
-#youtoob is this module slightly renamed: https://github.com/ridelore/willie-modules/blob/master/youtube.py
 import soundcloud
 from mutagen.easyid3 import EasyID3
 import requests
@@ -14,10 +12,50 @@ import random
 import string
 #what a mess
 
+def convert_date(date):
+    """Parses an ISO 8601 datestamp and reformats it to be a bit nicer"""
+    date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.000Z')
+    return date.strftime('%Y-%m-%d %H:%M:%S UTC')
 
+def convert_duration(duration):
+    """Converts an ISO 8601 duration to a human-readable duration"""
+    units = {
+        'hour': 0,
+        'minute': 0,
+        'second': 0
+    }
+    for symbol, unit in zip(('H', 'M', 'S'), ('hour', 'minute', 'second')):
+        match = re.search(r'(\d+)' + symbol, duration)
+        if match:
+            units[unit] = int(match.group(1))
+    time = datetime.time(**units)
+    output = str(time)
+    match = re.search('(\d+)D', duration)
+    if match:
+        output = match.group(1) + ' days, ' + output
+    return output
+
+def fetch_yt_video_info(bot,id):
+    url = "https://www.googleapis.com/youtube/v3/videos?key=API_KEY_GOES_HERE&part=contentDetails,status,snippet,statistics&id={0}".format(id)
+    results = requests.get(url).json()
+    video = results['items'][0]
+    info = {
+        'title': video['snippet']['title'],
+        'uploader': video['snippet']['channelTitle'],
+        'uploaded': convert_date(video['snippet']['publishedAt']),
+        'duration': convert_duration(video['contentDetails']['duration']),
+        'views': video['statistics'].get('viewCount') or '0',
+        'comments': video['statistics'].get('commentCount') or '0',
+        'likes': video['statistics'].get('likeCount') or '0',
+        'dislikes': video['statistics'].get('dislikeCount') or '0',
+        'link': 'https://youtu.be/' + video['id']
+    }
+    return info
+    
+    
 def soundcloudtitle(link):
-    client = soundcloud.Client(client_id='clientidgoeshere')
-    track = client.get('/resolve', url = link, client_id='clientidgoeshere')
+    client = soundcloud.Client(client_id='')
+    track = client.get('/resolve', url = link, client_id='')
     return "{0} - {1}".format(track.user['username'],track.title)
 
 
@@ -49,14 +87,14 @@ def sotd(bot, trigger):
             link = match.group(0)
             domain = urlparse(link)
             if(domain.netloc == 'youtu.be' or domain.netloc == 'www.youtu.be'):
-                yt = fetch_video_info(bot, domain.path[1:])
+                yt = fetch_yt_video_info(bot, domain.path[1:])
                 song = yt['title']
             elif(domain.netloc == 'youtube.com' or domain.netloc == 'www.youtube.com'):
-                yt = fetch_video_info(bot, domain.query[2:])
+                yt = fetch_yt_video_info(bot, domain.query[2:])
                 song = yt['title']
             elif(domain.netloc == 'soundcloud.com' or domain.netloc == 'www.soundcloud.com'):
                 song = soundcloudtitle(link)
-                elif(domain.netloc == 'i.sc0tt.net'):
+            elif(domain.netloc == 'i.sc0tt.net'):
                 filename = "/tmp/{0}".format(''.join(random.choice(string.ascii_lowercase) for i in range(10))+".mp3")
                 response = requests.head(link)
                 if (int(response.headers['Content-Length']) < 26214400):
@@ -70,7 +108,7 @@ def sotd(bot, trigger):
                         song = ""
                 else:
                     song = ""
-            else:    
+            else:
                 song = ""
             sdate = datetime.datetime.now()
             mysql(name, link, song, sdate,'insert')
@@ -79,9 +117,9 @@ def sotd(bot, trigger):
             return bot.say("Enter a valid link.")
     else:
         res = mysql(action='select')
-        bot.say("Last SotD: {0}".format(res[2]))
+        bot.say("Last SotD: {0} - {1}".format(res[2],res[3]))
 
 @sopel.module.commands('sotdweb')
 @sopel.module.commands('websotd')
 def sotdweb(bot,trigger):
-    bot.say("Website for history: website1 OR website2")
+    bot.say("Website for history: weblink1 OR weblink2")
