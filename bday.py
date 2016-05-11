1022505854581a1c52a5d776e555941792c98c73
dasu <desusp@gmail.com>
2016-05-11 11:42:52 -0400

    updated some stuff and add auto-announce
    announce feature untested.

--- bday.py
+++ bday.py
@@ -1,54 +1,53 @@
 from datetime import datetime
 import json
-import willie
+import sopel
 
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
 def date_hook(json_dict):
     for(key, value) in json_dict.items():
-        try: 
+        try:
             json_dict[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
         except:
             pass
     return json_dict
 
 def writejson(dict):
-    with open('/home/desu/phenny-master/modules/data.txt', 'w') as bdayfile:
+    with open('/home/desu/.sopel/data.txt', 'w') as bdayfile:
         json.dump(dict, bdayfile, default=date_handler)
 
 def readjson():
-    with open('/home/desu/phenny-master/modules/data.txt','r') as bdayfile:
+    with open('/home/desu/.sopel/data.txt','r') as bdayfile:
         dict = json.loads(bdayfile.read(), object_hook=date_hook)
     return dict
 
 def datetonext(dict):
-        res = []
-        today = datetime.today()
-        for x,y in dict.items():
-                delta = y.replace(year=(datetime.today().year)) - today
-                if delta.total_seconds() < 0:
-                        delta = y.replace(year=(datetime.today().year+1)) - today
-                res.append([x,delta])
-        res.sort(key=lambda x: x[1])
-        return res
+    res = []
+    today = datetime.today()
+    for x,y in dict.items():
+        delta = y.replace(year=(datetime.today().year)) - today
+        if delta.total_seconds() < 0:
+            delta = y.replace(year=(datetime.today().year+1)) - today
+        res.append([x,delta])
+    res.sort(key=lambda x: x[1])
+    return res
 
 def setbday(bot, trigger):
+    #disabled function
     dict = readjson()
     name = trigger.nick
-    #bot.say(trigger.group(1))
     try:
         date = datetime.strptime(trigger.group(1), '%m-%d')
     except:
-        bot.say("Please enter a valid date.  Accepts MONTH-DAY only.")
-        return
+        return bot.say("Please enter a valid date.  Accepts MONTH-DAY only.")
     dict[name] = date
-    #bot.say(name+str(date))
-    writejson(dict)    
+    writejson(dict)
     bot.say("Birthday saved.")
 #setbday.rule = r'^.setbday (\d{1,2}-\d{1,2})$'
 
 
-@willie.module.commands('bday','birthday')
+@sopel.module.commands('bday','birthday')
 def nextbday(bot, trigger):
     dict = readjson()
     res = datetonext(dict)
@@ -63,7 +62,6 @@ def nextbday(bot, trigger):
         for c in dict.items():
             if c[1].date() == datetime.today().replace(year=1904).date():
                 btoday = "Today is {0} birthday!".format(c[0])
-    
         nname=res[0][0]
         nbday=(dict[nname]).strftime('%B %d')
         daysaway=(res[0][1]).days + 1
@@ -71,3 +69,15 @@ def nextbday(bot, trigger):
             bot.say("{0} - Next birthday: {1} on {2} ({3} days away)".format(btoday, nname, nbday, daysaway))
         else:
             bot.say("Next birthday: {0} on {1} ({2} days away)".format(nname, nbday, daysaway))
+
+
+@sopel.module.interval(21600)
+def announce_bday(bot):
+    announce = []
+    dict = readjson()
+    res = datetonext(dict)
+    for people in res:
+        if people[1].days == 0:
+            announce.append(people[0])
+    if announce:
+        bot.say("Happy Birthday {0}!".format(", ".join(announce)))
