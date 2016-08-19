d4368439e2b01b8b92c48626d321b6a671ba7fdb
dasu <desusp@gmail.com>
2016-08-19 15:37:13 -0400

    Create nicovideourl.py
    info on nicovideo urls

---
+++ nicovideourl.py
@@ -0,0 +1,28 @@
+import random
+import sopel
+from sopel.tools import SopelMemory
+from nicovideo import Nicovideo
+import re
+
+nvregex = re.compile('.*(https?:\/\/(?:www\.)?nicovideo\.jp\/watch\/(.*?)((?=[\s])|$))')
+
+def setup(bot):
+    if not bot.memory.contains('url_callbacks'):
+        bot.memory['url_callbacks'] = SopelMemory()
+    bot.memory['url_callbacks'][nvregex] = nicovideoirc
+
+def shutdown(bot):
+    del bot.memory['url_callbacks'][nvregex]
+
+@sopel.module.rule('.*(https?:\/\/(?:www\.)?nicovideo\.jp\/watch\/(.*?)((?=[\s])|$))')
+def nicovideoirc(bot, trigger, match=None):
+    match = match or trigger
+    id = match.group(2)
+    nico = Nicovideo()
+    nico.append(id)
+    title = nico._video[id].title
+    seconds = nico._video[id].length
+    duration = '%d:%02d' % (seconds / 60, seconds % 60)
+    views = nico._video[id].view_counter
+    tags = ", ".join(random.sample(nico._video['sm29340570'].tags,3)) if len(nico._video['sm29340570'].tags) > 3 else ", ".join(nico._video['sm29340570'].tags)
+    bot.say(u"{} [{}]| ►:{} | Tags: {}".format(title,duration,views,tags))
