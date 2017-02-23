2d1e15ff7a96d4e1519c3eb5cfb8d601fe1a0e08
dasu <dasu@users.noreply.github.com>
2017-02-23 17:54:02 -0500

    Update soundcloudurl.py

--- soundcloudurl.py
+++ soundcloudurl.py
@@ -4,7 +4,7 @@ import soundcloud
 import re
 
 scregex = re.compile('.*(https?:\/\/soundcloud.com\/.*?\/.*?((?=[\s])|$))')
-
+client_id = '' #soundcloud client ID
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
         bot.memory['url_callbacks'] = SopelMemory()
@@ -15,30 +15,23 @@ def shutdown(bot):
 
 @sopel.module.rule('.*(https?:\/\/soundcloud.com\/.*?\/.*?((?=[\s])|$))')
 def soundcloudirc(bot, trigger, match=None):
-    client = soundcloud.Client(client_id='')
+    client = soundcloud.Client(client_id=client_id)
     match = match or trigger
     try:
-        track = client.get('/resolve', url=match.group(1), client_id='')
+        track = client.get('/resolve', url=match.group(1), client_id=client_id)
     except:
         return
+    title = track.title
+    artist = track.user['username']
+    hours, ms = divmod(track.duration, 3600000)
+    minutes, ms = divmod(ms, 60000)
+    seconds = float(ms)/1000
+    time = "%02i:%02i" % ( minutes, seconds) if hours is 0 else "%02i:%02i:%02i" % (hours, minutes, seconds)
+    genre = track.genre
     if track.kind == 'track':
-        title = track.title
-        artist = track.user['username']
         plays = track.playback_count
         favorites = track.favoritings_count
-        hours, ms = divmod(track.duration, 3600000)
-        minutes, ms = divmod(ms, 60000)
-        seconds = float(ms)/1000
-        time = "%02i:%02i" % ( minutes, seconds) if hours is 0 else "%02i:%02i:%02i" % (hours, minutes, seconds)
-        genre = track.genre
         bot.say(u"{0} - {1} [{2}] ►:{3} ❤:{4} Genre: {5}".format(artist,title,time,plays,favorites,genre))
     if track.kind == 'playlist':
-        title = track.title
-        artist = track.user['username']
-        hours, ms = divmod(track.duration, 3600000)
-        minutes, ms = divmod(ms, 60000)
-        seconds = float(ms)/1000
-        time = "%02i:%02i" % ( minutes, seconds) if hours is 0 else "%02i:%02i:%02i" % (hours, minutes, seconds)
         track_count = track.track_count
-        genre = track.genre
         bot.say(u"{0} - {1} [{2}] Tracks: {3} Genre: {4}".format(artist,title,time,track_count,genre))
