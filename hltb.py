a2d59ae610b65a9730691538aecc261b4b5b6305
dasu <dasu@users.noreply.github.com>
2018-05-17 10:26:34 -0400

    fix hltb
    a fugging s!!

--- hltb.py
+++ hltb.py
@@ -9,7 +9,7 @@ def hltb(bot,trigger):
     if not trigger.group(2):
         return bot.say("Enter a game name to search.")
     game = trigger.group(2)
-    url = "http://howlongtobeat.com/search_main.php?page=1"
+    url = "https://howlongtobeat.com/search_main.php?page=1"
     payload = {"queryString":game,"t":"games","sorthead":"popular","sortd":"Normal Order","length_type":"main","detail":"0"}
     test = {'Content-type':'application/x-www-form-urlencoded', 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36','origin':'https://howlongtobeat.com','referer':'https://howlongtobeat.com'}
     session = requests.Session()
