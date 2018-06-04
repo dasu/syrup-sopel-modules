40da05e637aac0d921e9a34797697bf58e45c597
dasu <dasu@users.noreply.github.com>
2018-06-04 16:04:17 -0400

    Update mtg.py
    deckbrew api shut down a month ago.

--- mtg.py
+++ mtg.py
@@ -1,24 +1,18 @@
-#card name, card type, mana cost, card effect, power +
-#toughness if the card type is a creature
-#from urllib.request import urlopen
 import requests
-import json
 import sopel
 
 @sopel.module.commands('mtg','magic')
 def mtg(bot,trigger):
     if not trigger.group(2):
         return bot.say("Please enter a card name.")
-    i = trigger.group(2)
-    x = requests.get('https://api.deckbrew.com/mtg/cards?name={0}'.format(i))
-    c = x.json()
-    if not c:
+    x = requests.get('https://api.magicthegathering.io/v1/cards?name={0}'.format(trigger.group(2))).json()
+    if not x['cards']:
         return bot.say('Card not found.')
-    js = c[0]
-    if 'creature' in js['types']:
+    js = x['cards'][0]
+    if 'Creature' in js['types']:
         if 'power' in js:
-            bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: {3}, Power: {4}, Toughness: {5}'.format(js['name'], js['types'], js['cost'], js['text'], js['power'], js['toughness']))
+            bot.say('{0} [{1}] {2}| {3} | {4}/{5}'.format(js['name'], js['type'], js.get('manaCost', ''), js.get('text',''), js['power'], js['toughness']))
         else:
-            bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: "{3}"'.format(js['name'], js['types'], js['cost'], js['text']))
+            bot.say('{0} [{1}] {2}| {3}'.format(js['name'], js['type'], js.get('manaCost',''), js.get('text','')))
     else:
-        bot.say('Name: {0}, Type: {1}, Cost: {2}, Effect: "{3}"'.format(js['name'], js['types'], js['cost'], js['text']))
+        bot.say('{0} [{1}] {2}| {3}'.format(js['name'], js['type'], js.get('manaCost', ''), js.get('text','')))
