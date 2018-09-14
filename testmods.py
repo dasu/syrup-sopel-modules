7ada3b2d8a809308afaba124807c5a01c6968976
dasu <dasu@users.noreply.github.com>
2018-09-14 16:14:42 -0400

    add twitch OCR test
    literally untested in a real world setting

--- testmods.py
+++ testmods.py
@@ -4,6 +4,12 @@ from sopel.tools import Identifier
 from sopel.tools import SopelMemory
 import re
 
+#zfg - below can be removed after graduation
+import streamlink
+import cv2
+import pytesseract
+#endzfg
+
 ssthisregex = re.compile('.*\W(?:ss|screenshot) this.*')
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
@@ -21,6 +27,18 @@ def testusers(bot, trigger):
                 bot.say("it matched :(")
         else:
                 bot.say(line)
+
+@sopel.module.commands('userquery')
+def userquery(bot,trigger):
+    person = trigger.group(2)
+    nicklist = []
+    nicks = bot.channels[trigger.sender.lower()].users
+    for nick in nicks:
+        nicklist.append(nick)
+    if trigger.group(2) in nicklist:
+        bot.say('ya')
+    else:
+        bot.say('nope')
 
 @sopel.module.commands('wittest')
 def wittest(bot,trigger):
@@ -50,3 +68,33 @@ def lewd(bot,trigger):
             bot.say("[NSFW] wow lewd... lewd score: %.3f/1.00"%js['adult']['adultScore'])
         else:
             bot.say("phew safe... lewd score: %.3f/1.00"%js['adult']['adultScore'])
+
+@sopel.module.commands('hostm')
+def hostm(bot,trigger):
+    bot.say(trigger.host)
+
+@sopel.module.commands('zfg')
+def zfg(bot, trigger):
+    session = streamlink.Streamlink()
+    session.set_plugin_option("twitch", "disable_hosting", True)
+    st = session.streams("https://twitch.tv/zfg1")
+    if not st:
+      return bot.say("zfg isn't streaming :(")
+    s = st['best']
+    fd = s.open()
+    data = fd.read(int(5e6))
+    fd.close()
+    fname = '/home/desu/test/s.bin'
+    open(fname, 'wb').write(data)
+    capture = cv2.VideoCapture(fname)
+    imgdata = capture.read()[1]
+    i = imgdata[...,::-1]
+    fc = cv2.cvtColor(i, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('/home/desu/htdocs/desu/zfg/captured_frame.jpg', fc)
+
+    timer_crop = fc[427:465, 179:331]
+    cv2.imwrite('/home/desu/htdocs/desu/zfg/timer_crop.jpg', timer_crop)
+    rows, cols,_ = timer_crop.shape
+    tc_r = cv2.resize(timer_crop, (int(cols*4), int(rows*4)))
+    timer_string = pytesseract.image_to_string(tc_r)
+    bot.say("Time: {} | Estimated time to Dampe: {} | Images: https://dasu.moe/desu/zfg/".format(timer_string, "Not Implemented Yet"))
