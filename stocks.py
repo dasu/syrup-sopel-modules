578a09f155965c396745d384db8159ecc456597b
dasu <dasu@users.noreply.github.com>
2018-09-20 15:47:57 -0400

    add a rate limit message instead of keyerror.

--- stocks.py
+++ stocks.py
@@ -11,6 +11,8 @@ def stocks(bot,trigger):
         x = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=^DJI&interval=1min&apikey=APIKEYGOESHERELOL")
     else:
         x = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&interval=1min&apikey=APIKEYGOESHERELOL".format(trigger.group(2)))
+    if x.json().get('Information'):
+        return bot.say('Rate limit reached (5/min), try again in one minute')
     if x.json().get('Error Message'):
         return bot.say('Please enter a valid stock symbol')
     if trigger.group(2):
