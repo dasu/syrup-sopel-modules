ce68ffb77443eeb4f25d9edc18f1c27d9d797c41
dasu <dasu@users.noreply.github.com>
2019-10-28 10:34:43 -0400

    add pblast: sends last sent url to pushbullet.

--- pb.py
+++ pb.py
@@ -7,3 +7,14 @@ def pushbullet(bot, trigger):
         url = trigger.group(2)
         pb = Pushbullet('API_KEY_GOES_HERE')
         pb.push_link(url,url)
+
+@sopel.module.commands('pblast')
+def pushbulletlast(bot,trigger):
+    if trigger.group(2):
+        return
+    if trigger.nick == 'NICK_GOES_HERE' or trigger.nick == 'NICK_GOES_HERE':
+        if trigger.sender not in bot.memory['last_seen_url']:
+            return
+        url = bot.memory['last_seen_url'][trigger.sender]
+        pb = Pushbullet('API_KEY_GOES_HERE')
+        pb.push_link(url,url)
