2b5217e5aaa48a5c0fc7be2db60c0f42254ad70a
dasu <dasu@users.noreply.github.com>
2022-03-09 22:44:26 -0500

    update for new twitch api

--- twitch.py
+++ twitch.py
@@ -1,5 +1,6 @@
-#As of 09-14-2016, twitch API now requires Client-ID with all requests.  You will need to create one from here: 
-# https://www.twitch.tv/settings/connections
+# As of 02-28-2022, twitch api moved over to new api, and requires a client ID and client secret key which can be generated from here:
+# https://dev.twitch.tv/
+# more info on new api here: https://dev.twitch.tv/docs/api/migration
 # complete twitch.py rewrite coming soon™
 import requests
 import sopel
@@ -8,9 +9,11 @@ from sopel.tools import SopelMemory
 import datetime
 
 # TODO: Make these config options c:
-twitchclientid = "clientIDgoeshere"
+twitchclientid = "twitchclientidgoeshere"
+twitchclientsecret = "twitchclientsecretgoeshere"
+token = "" #gets populated on startup, and refreshed occasionally
 announce_chan = "#pancakes"
-logchannel = "#whateverchannelyouwant" #used for live logging certain issues that plague this module
+logchannel = "#dasu" #used for live logging certain issues that plague this module
 streamers = [
 "31847612", #coalll
 "22725500", #kwlpp
@@ -19,19 +22,24 @@ streamers = [
 "44191385", #repppie
 "24683935", #supersocks
 "26979782", #sc00ty
-"37155900", #kaask
-"126482446" #mogra untz untz
+"37155900"  #kaask
 ]
 #end config
 
 twitchregex = re.compile('.*https?:\/\/(?:(?:www\.)|(?:go\.))?twitch.tv\/(.*?)\/?(?:(?=[\s|\/])|$)')
 twitchclipsregex = re.compile('.*https?:\/\/clips\.twitch.tv\/(.*?)\/?(?:(?=[\s])|$)')
 
+def tokensetup(bot, twitchclientid, twitchclientsecret):
+    x = requests.post('https://id.twitch.tv/oauth2/token?client_id={}&client_secret={}&grant_type=client_credentials'.format(twitchclientid,twitchclientsecret))
+    global token
+    token = x.json()['access_token']
+
 def setup(bot):
     if not bot.memory.contains('url_callbacks'):
         bot.memory['url_callbacks'] = SopelMemory()
     bot.memory['url_callbacks'][twitchregex] = twitchirc
     bot.memory['url_callbacks'][twitchclipsregex] = twitchclipsirc
+    tokensetup(bot,twitchclientid,twitchclientsecret)
 
 def shutdown(bot):
     del bot.memory['url_callbacks'][twitchregex]
@@ -43,24 +51,24 @@ tsep = lambda a : "{:,}".format(int(a))
 
 def twitch_api(stream_list):
   """ Returns the result of the http request from Twitch's api """
-  return requests.get('https://api.twitch.tv/kraken/streams', params={"channel": ",".join(stream_list)}, headers={"Client-ID": twitchclientid,"Accept":"application/vnd.twitchtv.v5+json"}).json()
+  return requests.get('https://api.twitch.tv/helix/streams?{}'.format('&'.join(map('user_id={}'.format,stream_list))), headers={"Client-ID": twitchclientid, "Authorization":"Bearer {}".format(token)}).json()
 
 def gettwitchuserid(tuser):
-    tuid = requests.get("https://api.twitch.tv/kraken/users?login={}".format(tuser),headers={"Client-ID":twitchclientid,"Accept":"application/vnd.twitchtv.v5+json"}).json()
-    if not tuid['users']:
+    tuid = requests.get("https://api.twitch.tv/helix/users?login={}".format(tuser),headers={"Client-ID":twitchclientid,"Authorization":"Bearer {}".format(token)}).json()
+    if not tuid['data']:
       return None
     else:
-      return [tuid['users'][0]['_id']]
+      return [tuid['data'][0]['id']]
 
 def twitch_generator(streaming):
-  for streamer in streaming["streams"]:
+  for streamer in streaming["data"]:
     streamer_dict = {}
-    streamer_dict["name"] = streamer["channel"]["name"]
-    streamer_dict["game"] = streamer["channel"]["game"]
-    streamer_dict["status"] = streamer["channel"]["status"]
-    streamer_dict["url"] = streamer["channel"]["url"]
-    streamer_dict["starttime"] = datetime.datetime.strptime(streamer['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-    streamer_dict["viewers"] = streamer["viewers"]
+    streamer_dict["name"] = streamer["user_name"]
+    streamer_dict["game"] = streamer["game_name"]
+    streamer_dict["status"] = streamer["title"]
+    streamer_dict["url"] = "https://twitch.tv/{}".format(streamer["user_login"])
+    streamer_dict["starttime"] = datetime.datetime.strptime(streamer['started_at'], '%Y-%m-%dT%H:%M:%SZ')
+    streamer_dict["viewers"] = streamer["viewer_count"]
     yield streamer_dict
 
 @sopel.module.interval(20)
@@ -71,7 +79,7 @@ def monitor_streamers(bot):
   except Exception as  e:
     return print("There was an error reading twitch API  {0}".format(e))
   results = []
-  if streaming.get("streams"):
+  if streaming.get("data"):
     twitch_gen = twitch_generator(streaming)
     for streamer in twitch_gen:
       if streamer["name"] not in currently_streaming:
@@ -86,7 +94,7 @@ def monitor_streamers(bot):
 
   if results:
     bot.msg(announce_chan, ", ".join(results))
-
+    #bot.msg(logchannel,"announced {}".format(streamer["name"]))
   # Remove people who stopped streaming
   for streamer in list(currently_streaming):
     if streamer not in streaming_names:
@@ -107,7 +115,7 @@ def streamer_status(bot, trigger):
   except Exception as  e:
     return print("There was an error reading twitch API  {0}".format(e))
   results = []
-  if streaming.get("streams"):
+  if streaming.get("data"):
     twitch_gen = twitch_generator(streaming)
     for streamer in twitch_gen:
       results.append("%s is playing %s [%s] (%s - %s viewer%s)" % (streamer["name"],
@@ -131,7 +139,7 @@ def twitchirc(bot, trigger, match = None):
   except Exception as  e:
     return print("There was an error reading twitch API  {0}".format(e))
   results = []
-  if streaming.get("streams"):
+  if streaming.get("data"):
     twitch_gen = twitch_generator(streaming)
     for streamer in twitch_gen:
       results.append("%s is playing %s [%s] (%s - %s viewer%s)" % (streamer["name"],
@@ -149,11 +157,11 @@ def twitchirc(bot, trigger, match = None):
 def twitchclipsirc(bot,trigger, match = None):
   match = match or trigger
   slug = match.group(1) or match.group(2)
-  clips = requests.get("https://api.twitch.tv/kraken/clips/{}".format(slug), headers={"Client-ID":twitchclientid,"Accept":"application/vnd.twitchtv.v5+json"}).json()
-  name = clips['broadcaster']['display_name']
-  title = clips['title']
-  game = clips['game']
-  views = clips['views']
+  clips = requests.get("https://api.twitch.tv/helix/clips?id={}".format(slug), headers={"Client-ID": twitchclientid, "Authorization":"Bearer {}".format(token)}).json()
+  name = clips['data'][0]['broadcaster_name']
+  title = clips['data'][0]['title']
+  game = clips['data'][0]['game_id'] #need to add gameid->game name function
+  views = clips['data'][0]['view_count']
   bot.say("{} [{}] | {} | {} views".format(title, game, name, tsep(views)))
 
 @sopel.module.commands('debugtv')
